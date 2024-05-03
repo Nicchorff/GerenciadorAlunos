bccfaa78f659913c1183e2b89ef22ad60a4fe36c
Nicchorff <pedrohkerkoff123@gmail.com>
2024-05-03 17:46:58 -0300

    Usando orednacao com TreeSet

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,2 +1,72 @@
-package PACKAGE_NAME;public class Aluno {
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Aluno implements Comparable<Aluno> {
+    private String nome;
+    private int matricula;
+    private double nota;
+
+    public Aluno(String nome, int matricula, double nota) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return getMatricula() == aluno.getMatricula();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getMatricula());
+    }
+
+    @Override
+    public String toString() {
+        return "{" + nome + '\'' +
+                ", " + matricula +
+                ", " + nota +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Aluno o) {
+        return nome.compareToIgnoreCase(o.getNome());
+    }
+}
+
+class ExibirAlunosPorNota implements Comparator<Aluno>{
+
+    @Override
+    public int compare(Aluno o1, Aluno o2) {
+        return Double.compare(o1.getNota(), o2.getNota());
+    }
 }
--- src/GerenciadorAlunos.java
+++ src/GerenciadorAlunos.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class GerenciadorAlunos {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class GerenciadorAlunos {
+    private Set<Aluno> alunos;
+
+    public GerenciadorAlunos() {
+        this.alunos = new HashSet<>();
+    }
+
+    public void addAluno(String nome, int matricula, double nota){
+        alunos.add(new Aluno(nome, matricula, nota));
+    }
+
+    public void removerAluno(int matricula){
+        Aluno aluno = null;
+        for(Aluno a : alunos){
+            if(a.getMatricula() == matricula){
+                aluno = a;
+                break;
+            }
+        }
+        alunos.remove(aluno);
+    }
+
+    public Set<Aluno> exibirAlunosPorNome(){
+        Set<Aluno> listaDeAlunos = new TreeSet<>(alunos);
+        return listaDeAlunos;
+    }
+    public Set<Aluno> exibirAlunosPorNota(){
+        Set<Aluno> listaDeAlunos = new TreeSet<>(new ExibirAlunosPorNota());
+        listaDeAlunos.addAll(alunos);
+        return listaDeAlunos;
+    }
 }
+
